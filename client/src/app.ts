bbaa1d5d47597225c71f524b6d79ae7aac2468af
Yusuke Endoh <mame@ruby-lang.org>
2020-08-02 01:40:35 +0900

    Fix the spec change of require

--- client/src/app.ts
+++ client/src/app.ts
@@ -2,7 +2,7 @@ import "./index.html";
 import "./app.scss";
 import {AI} from "./ai";
 import {UI} from "./ui";
+//@ts-ignore
+import ai_txt from "ai.txt";
 
-declare function require(s: string): string;
-
-new UI(new AI(require("ai.txt")));
+new UI(new AI(ai_txt));
